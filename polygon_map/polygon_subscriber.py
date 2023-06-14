d8a2bef69e7fb58f09e99ba4fdb768d6890b38ac
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2023-06-14 11:01:34 +0200

    optionally read output channel from environment variable

--- polygon_map/polygon_subscriber.py
+++ polygon_map/polygon_subscriber.py
@@ -6,26 +6,29 @@ import os
 from .PolygonStampedMapSubscriber import PolygonStampedMapSubscriber
 
 API_URL = os.environ.get('PARTITION_API_URL')
+MY_ODOM_TOPIC = os.environ.get('MY_ODOM_TOPIC')
+OTHER_ODOM_TOPICS = os.environ.get('OTHER_ODOM_TOPICS')
+OUTPUT_CHANNEL = os.environ.get('OUTPUT_CHANNEL', '/assigned_border')
+
 if not API_URL:
     print('PARTITION_API_URL environment variable not set.')
     print('Cannot access the API as a result.')
     print('Exiting.')
     exit(1)
 
-MY_ODOM_TOPIC = os.environ.get('MY_ODOM_TOPIC')
 if not MY_ODOM_TOPIC:
     print('MY_ODOM_TOPIC environment variable not set.')
     print('Cannot retrieve robot location as a result.')
     print('Exiting.')
     exit(2)
 
-OTHER_ODOM_TOPICS = os.environ.get('OTHER_ODOM_TOPICS')
 if not OTHER_ODOM_TOPICS:
     print('OTHER_ODOM_TOPICS environment variable not set.')
-    print('Cannot retrieve robot location as a result.')
+    print('Cannot retrieve robot locations as a result.')
     print('Exiting.')
     exit(3)
 else:
+    # topics must be in a space separated string
     OTHER_ODOM_TOPICS = OTHER_ODOM_TOPICS.split()
 
 
@@ -34,7 +37,7 @@ def main(args=None):
 
     sub = PolygonStampedMapSubscriber(
         input_channel='/exploration_zone',
-        output_channel='/assigned_border',
+        output_channel=OUTPUT_CHANNEL,
         api_url=API_URL,
         api_route='PolygonToCellMapContours',
         my_odom_topic=MY_ODOM_TOPIC,
